29be25561f311b834ff08721ff3a45b63f6c379d
Flora Guibon <fguibon@excilys.com>
2019-04-15 16:57:09 +0200

    First

---
+++ src/model/Computer.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class Computer {
+
+	private int id;
+	private String name;
+	private Timestamp introduced;
+	private Timestamp discontinued;
+	private int id_company;
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the introduced
+	 */
+	public Timestamp getIntroduced() {
+		return introduced;
+	}
+	/**
+	 * @param introduced the introduced to set
+	 */
+	public void setIntroduced(Timestamp introduced) {
+		this.introduced = introduced;
+	}
+	/**
+	 * @return the discontinued
+	 */
+	public Timestamp getDiscontinued() {
+		return discontinued;
+	}
+	/**
+	 * @param discontinued the discontinued to set
+	 */
+	public void setDiscontinued(Timestamp discontinued) {
+		this.discontinued = discontinued;
+	}
+	/**
+	 * @return the id_company
+	 */
+	public int getId_company() {
+		return id_company;
+	}
+	/**
+	 * @param id_company the id_company to set
+	 */
+	public void setId_company(int id_company) {
+		this.id_company = id_company;
+	}
+	
+}
