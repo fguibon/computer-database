33999aa71ca201efd226c692aa8bdc1e086b6a15
Flora Guibon <fguibon@excilys.com>
2019-04-29 12:34:37 +0200

    [UPDATE] The dashboard(static) is displayed

--- src/main/java/com/excilys/servlet/DashboardServlet.java
+++ src/main/java/com/excilys/servlet/DashboardServlet.java
@@ -3,7 +3,6 @@ package com.excilys.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,22 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 public class DashboardServlet extends HttpServlet {
 
 
+	private static final long serialVersionUID = -4872201161261134158L;
+
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 
 			throws ServletException, IOException {
-
-		res.setContentType("text/html");
-
-		ServletOutputStream out = res.getOutputStream();
-
-		out.println("<HTML>\n");
-		out.println("<HEAD>\n");
-		out.println("<TITLE>Bonjour</TITLE>\n");
-		out.println("</HEAD>\n");
-		out.println("<BODY>\n");
-		out.println("<H1>Bonjour</H1>\n");
-		out.println("</BODY>\n");
-		out.println("</HTML>");
+		//req.getContextPath();
 
 	}
 
